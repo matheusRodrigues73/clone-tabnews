126a852e6c2bed1610e24bcb838326d7e5ffa4ac
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2023-10-11 16:17:41 +0000

    adiciona pasta pages

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <p>Muito obrigadi por ser a pessoa incrivel que vc e sempre ser uma otima amiga desde a escola, agradeço muito a Deus por ter voce como amiga. Saiba que quando precisar pode sempre contar comigo ❤️.</p>
+}
+
+export default Home
