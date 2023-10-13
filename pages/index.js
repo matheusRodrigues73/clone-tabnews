1554621392c3869ac5bceddc9df6e4f0e01ad1d1
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2023-10-13 18:53:15 +0000

    Mensagem corrigida

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <p>Muito obrigadi por ser a pessoa incrivel que vc e sempre ser uma otima amiga desde a escola, agradeço muito a Deus por ter voce como amiga. Saiba que quando precisar pode sempre contar comigo ❤️.</p>
+    return <p>Muito obrigado por ser a pessoa incrível que você é e sempre ser uma otima amiga desde a escola, agradeço muito a Deus por ter você comigo. Saiba que quando precisar eu sempre vou estar aqui ❤️</p>
 }
 
 export default Home
