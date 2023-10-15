227e45612de7f1a6cf44c6be910c0ff93dbdf0d8
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2023-10-15 17:11:52 +0000

    Configura o `Prettier` e o `.prettierignore`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendendo programação com um projeto completo recriando o https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-function Home(){
-  return <h1>Muito obrigado por ser a pessoa incrível que você é e sempre ser uma otima amiga desde a escola, agradeço muito a Deus por ter você comigo. Saiba que quando precisar eu sempre vou estar aqui ❤️</h1>
+function Home() {
+  return (
+    <h1>
+      Muito obrigado por ser a pessoa incrível que você é e sempre ser uma otima
+      amiga desde a escola, agradeço muito a Deus por ter você comigo. Saiba que
+      quando precisar eu sempre vou estar aqui ❤️
+    </h1>
+  );
 }
 
-export default Home
+function teste() {
+  console.log("formatação errada");
+}
+
+export default Home;
