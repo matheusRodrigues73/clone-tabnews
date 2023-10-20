4d764a522d5bba8811fa0534cf8b746ae933d6b1
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2023-10-20 13:24:24 +0000

    Atualiza menssagem da `Home`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 function Home() {
   return (
-    <h1>
-      Aproveite o processo pois ele é quem da sentido ao fim aguarde e vera as
-      realizações
-    </h1>
+    <div>
+      <h1>
+        Aproveite o processo pois ele é quem da sentido ao fim aguarde e vera as
+        realizações
+      </h1>
+      <p>
+        Este site esta sendo construido para ser um site pessoal, baseado na
+        arquitetura do site{" "}
+        <a href="https://www.tabnews.com.br" target="_blank">
+          tabnews
+        </a>
+        . Embreve tudo estará pronto!
+      </p>
+    </div>
   );
 }
 
