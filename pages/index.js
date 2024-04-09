1cac9acafe721c86ea38ee15baa5f57c67457fc2
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2024-04-09 20:43:59 +0000

    Adiciona a pasta pages

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <h1>
+      Na caminhada com Cristo surgem muitos desafios e lutas vão vir então aqui
+      estão algumas passagens da biblia preparadas especificamente para quando
+      voce estiver nesses momentos de grande dificuldade ou desanimo:
+    </h1>
+  );
+}
+
+export default Home;
