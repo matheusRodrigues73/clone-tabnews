31ce88031a4ba774e3a91f7027134b3a388138a9
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2024-04-09 20:43:59 +0000

    Adiciona a pasta pages

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Que Cristo sempre seja sua luz guiando seu caminho e que em qualquer
+      momento dificil que Deus seja sua fortaleza.
+    </h1>
+  );
+}
+
+export default Home;
