5965bbfda5a0fc75538eab6d77e1737219cf0fd3
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2024-04-10 21:45:35 +0000

    Atualiza a frase para a Duda

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 function Home() {
   return (
-    <h1>
-      Que Cristo sempre seja sua luz guiando seu caminho e que em qualquer
-      momento dificil que Deus seja sua fortaleza.
-    </h1>
+    <>
+      <p>
+        Siga sempre enfrente no caminho que voce tem trilhado, sempre estudando
+        bastante, vivendo experiencias incríveis e nunca se esquecendo de servir
+        a Cristo. Oro para que Deus esteja cuidando de voce, por fim uma
+        menssagem, quando que estiver passando por tempos dificeis se lembre
+        disso:
+      </p>
+      <blockquote>
+        <q>
+          Seja forte e corajoso! Não se apavore nem desanime, pois o Senhor, o
+          seu Deus, estará com você por onde você andar.
+        </q>{" "}
+        Josue 1.9
+      </blockquote>
+    </>
   );
 }
 
