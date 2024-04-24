0c036656e18338ce23a5f21adfb1cd3b0ea158a3
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2024-04-24 23:24:14 +0000

    Adiciona Pagina em Construção

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,73 @@
+import Link from "next/link";
+
 function Home() {
   return (
-    <>
-      <h1>Caça ao tesouro</h1>
-      <p>
-        Resolva o enigma e ganhe um premio mas cuidado! a cada dia que se passa
-        o premio diminui...
-      </p>
-      <blockquote>
-        <em>
-          "em minha (carteira azul) guardo meu tesouro às 12 chaves mas ela só
-          fala ingles:
-          <ol>
-            <li>joy</li>
-            <li>(sirene)</li>
-            <li>(habito)</li>
-            <li>moral</li>
-            <li>ketchup</li>
-            <li>(franja)</li>
-            <li>pact</li>
-            <li>(procura)</li>
-            <li>patch</li>
-            <li>(piloto)</li>
-            <li>soccer</li>
-            <li>install</li>
-          </ol>
-          <p>Quem a instalar podera ficar com meu tesouro!"</p>
-        </em>
-      </blockquote>
-    </>
+    <main className="min-h-screen">
+      <div className="flex justify-end gap-3 bg-blue-950 text-white">
+        <Link href={"https://www.instagram.com/theuz_2104/"} className="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            fill="currentColor"
+            className="bi bi-instagram"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+          </svg>
+        </Link>
+        <Link href={"https://github.com/matheusRodrigues73"} className="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            fill="currentColor"
+            className="bi bi-github"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+          </svg>
+        </Link>
+      </div>
+      <div className="bg-blue-950 grid pt-7 pb-14">
+        <p className="justify-self-center text-white text-3xl">WebClassroom.</p>
+      </div>
+      <div className="max-w mx-auto">
+        <h1 className="text-center py-3 text-4xl text-stone-500">
+          Uma Nova Experiência de{" "}
+          <b className="text-blue-400 py-0.5 border-b-2 border-blue-400">
+            Aprender
+          </b>
+        </h1>
+        <p className="text-lg text-justify px-2">
+          Um novo pedaço de internet está sendo construído para ser um local
+          onde organizar e armazenar trabalhos escolares ou acadêmicos, além de
+          poder <b>disponibilizar publicamente</b> o conhecimento gerado para
+          qualquer outro usuário acessar dentro da plataforma. Feito para
+          Educadores, Alunos e pessoas que buscam desenvolvimento constante,
+          terem um espaço reservado para consumir{" "}
+          <b>conteúdos de valor concreto</b> e contribuir para o avanço da
+          educação. O objetivo é aproximar as pessoas, incentivando o debate e o
+          compartilhamento de ideias de forma saudável, e tornar isso uma{" "}
+          <b>experiência agradável</b>.
+        </p>
+      </div>
+
+      <div className="py-8">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="56"
+          height="56"
+          fill="currentColor"
+          className="bi bi-exclamation-triangle w-full text-center"
+          viewBox="0 0 16 16"
+        >
+          <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z" />
+          <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+        </svg>
+        <b className="block text-center">Building Project...</b>
+      </div>
+    </main>
   );
 }
 
