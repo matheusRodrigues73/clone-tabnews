e37fb966bd3a6a7e8b09ce518ba0e2e2d04b14f1
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2024-05-08 22:24:16 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
 // Qualquer endpoint dentro da pasta api usando Next, automaticament de torna uma rota publica da api que terá uma função designada para receber requests do lado de fora e retornar uma response
 
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response
     .status(200)
     .json({ chave: "Alunos do curso.dev são acima da média" });
