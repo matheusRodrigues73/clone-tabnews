afe6ba74993956c085ae2b2885ac1857e2fa4b7a
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2024-05-29 23:41:41 +0000

    Finish endpoin `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,9 +3,37 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  // SHOW key-word is used to access database configuration properties
+
+  // server_version = "16.0" postgres version
+  const versionResponse = await database.query("SHOW server_version;");
+  const serverVersion = versionResponse.rows[0].server_version;
+
+  // max_connections are defined in configuration file too, thereform you can access that using SHOW key-word
+  const maxConnectionsResponse = await database.query("SHOW max_connections;");
+  const maxConnections = Number(maxConnectionsResponse.rows[0].max_connections);
+
+  // opened connections are not accessible by configurations file, it depends of the situation, or better, the status of connections;
+  // pg_stat is a subdirectory on postgres process and have the status informations, variable values, about the current situation of database
+  // count(*) from pg_stat_activity will serve for us the opened connections, specifing the database where we have seen;
+
+  const openedConnectionsResponse = await database.query(
+    "SELECT count(*) AS opened_connections FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const openedConnections = Number(
+    openedConnectionsResponse.rows[0].opened_connections,
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: serverVersion,
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET to api/v1/status should return 200", async () => {
   let response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const databaseInfo = responseBody.dependencies.database;
+
+  expect(databaseInfo.version).toEqual("16.0");
+
+  expect(typeof databaseInfo.max_connections).toBe("number");
+  expect(databaseInfo.max_connections).toEqual(100);
+
+  expect(typeof databaseInfo.opened_connections).toBe("number");
+  expect(
+    databaseInfo.opened_connections > 0 &&
+      databaseInfo.opened_connections < databaseInfo.max_connections,
+  ).toBeTruthy();
 });
