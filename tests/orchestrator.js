07e632a325d1db7c6ac0bb4ca73e6219fe3ce27a
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2024-07-04 17:42:21 -0300

    improve `test` script

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,11 +6,16 @@ async function waitForAllServices() {
   async function waitForWebServices() {
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
+      minTimeout: 500,
     });
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+
+      if (!response.ok) {
+        throw Error();
+      }
     }
   }
 }
