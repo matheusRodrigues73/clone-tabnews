9c8f985f246216e71f34d47ad97e494fe6dda516
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2024-09-18 22:18:35 +0000

    refactor: use `orchestrator.clearDatabase()` in tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
 
 describe('GET "api/v1/migrations"', () => {
   describe("Anonymous User", () => {
-    test("Running pending migrations on dry run mode", async () => {
+    test("Retriving pending migrations", async () => {
       let response = await fetch("http://localhost:3000/api/v1/migrations");
       expect(response.status).toBe(200);
       const responseBody = await response.json();
